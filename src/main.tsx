99ca27002735c8c33249af12e29da7607f24b777
Cristina-parisenti <crislu88@hotmail.com>
2026-08-20 15:47:56 -0300

    Ativar correção final da área do atleta

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from './App';
 import './styles.css';
 import './athleteHomeRefinement.css';
 import './athleteHomeRefinement';
+import './athleteHomeFinalFix';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
