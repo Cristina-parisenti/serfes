4fe69e3f94a6453b96b49c35b6db0c13c4b0d3a0
Cristina-parisenti <crislu88@hotmail.com>
2026-08-21 10:02:30 -0300

    Ativar bloqueio por validação do cadastro

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import './athleteProfileImageSafe';
 import './profileImageGuidance';
 import './registrationCardPresentationFix';
 import './athleteSexAndGreeting';
+import './athleteApprovalGate';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
